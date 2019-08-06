db2d516ea2785dc4c5a964acae5bbe98cf9529fb
Memo Cachu <gcachu.o@gmail.com>
2019-08-06 17:28:44 -0500

    Revert "changed extensions"
    This reverts commit f5268e7c06f69cb018ba63ffe2def21ea9eb3915.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,25 +7,25 @@ mkdir -p ../www
 
 ln -s ../api ../www/api
 
-cp config/files/components/template.index.tsx components/index.tsx
-cp config/files/components/template.app.tsx components/app.tsx
-cp config/files/interfaces/template.toastr.ts interfaces/toastr.ts
-cp config/files/styles/template.app.scss styles/app.scss
+cp config/files/components/index.tsx.template components/index.tsx
+cp config/files/components/app.tsx.template components/app.tsx
+cp config/files/interfaces/toastr.ts.template interfaces/toastr.ts
+cp config/files/styles/app.scss.template styles/app.scss
 
-cp config/files/www/template.htaccess ../www/.htaccess
-cp config/files/www/template.index.html ../www/index.html
-cp config/files/www/template.gitignore ../www/.gitignore
+cp config/files/www/.htaccess.template ../www/.htaccess
+cp config/files/www/index.html.template ../www/index.html
+cp config/files/www/.gitignore.template ../www/.gitignore
 
-cp config/files/template.gitignore .gitignore
-cp config/files/template.imports.ts imports.ts
-cp config/files/template.tsconfig.json tsconfig.json
-cp config/files/template.webpack.config.js webpack.config.js
+cp config/files/.gitignore.template .gitignore
+cp config/files/imports.ts.template imports.ts
+cp config/files/tsconfig.json.template tsconfig.json
+cp config/files/webpack.config.js.template webpack.config.js
 
 FILE=package.json
 if [ -f "$FILE" ]; then
     echo "$FILE exists"
 else
-    cp config/files/template.package.json package.json
+    cp config/files/package.json.template package.json
     read -p "Enter the project's name:" project
     sed -i "s/###PROJECT###/$project/g" package.json
     read -p "Enter the BASENAME:" BASENAME
