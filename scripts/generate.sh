6484c288ffed851eb62829f4666fb4aa46190010
Memo Cachu <gcachu.o@gmail.com>
2019-08-06 16:56:12 -0500

    more files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,11 +1,18 @@
 cd ..
 
-cp config/files/template.package.json package.json
-cp config/files/template.gitignore .gitignore
+cp config/files/.gitignore.template .gitignore
+cp config/files/imports.ts.template imports.ts
+cp config/files/tsconfig.json.template tsconfig.json
 
-read -p "Enter the project's name:" project
-sed -i "s/###PROJECT###/$project/g" package.json
-read -p "Enter the BASENAME:" BASENAME
-sed -i "s/###BASENAME###/\/$BASENAME/g" package.json
+FILE=package.json
+if [ -f "$FILE" ]; then
+    echo "$FILE exists"
+else
+    cp config/files/package.json.template package.json
+    read -p "Enter the project's name:" project
+    sed -i "s/###PROJECT###/$project/g" package.json
+    read -p "Enter the BASENAME:" BASENAME
+    sed -i "s/###BASENAME###/\/$BASENAME/g" package.json
+fi
 
 yarn
