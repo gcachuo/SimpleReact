27701c216a27ab38aff00d6e18397c4b38a23b6d
Memo Cachu <gcachu.o@gmail.com>
2019-08-06 17:08:43 -0500

    more files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,14 @@
 cd ..
 
+mkdir -p components/pages
+mkdir -p interfaces
+mkdir -p styles/components
+
+cp config/files/components/index.tsx.template components/index.tsx
+cp config/files/components/app.tsx.template components/app.tsx
+cp config/files/interfaces/toastr.ts.template interfaces/toastr.ts
+cp config/files/styles/app.scss.template styles/app.scss
+
 cp config/files/.gitignore.template .gitignore
 cp config/files/imports.ts.template imports.ts
 cp config/files/tsconfig.json.template tsconfig.json
