c71920e97f9e7dc0087d0bffa4ac284aa23da126
Memo Cachu <gcachu.o@gmail.com>
2019-08-06 17:21:03 -0500

    public files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,15 +3,23 @@ cd ..
 mkdir -p components/pages
 mkdir -p interfaces
 mkdir -p styles/components
+mkdir -p ../www
+
+ln -s ../api ../www/api
 
 cp config/files/components/index.tsx.template components/index.tsx
 cp config/files/components/app.tsx.template components/app.tsx
 cp config/files/interfaces/toastr.ts.template interfaces/toastr.ts
 cp config/files/styles/app.scss.template styles/app.scss
 
+cp config/files/www/.htaccess.template ../www/.htaccess
+cp config/files/www/index.html.template ../www/index.html
+cp config/files/www/.gitignore.template ../www/.gitignore
+
 cp config/files/.gitignore.template .gitignore
 cp config/files/imports.ts.template imports.ts
 cp config/files/tsconfig.json.template tsconfig.json
+cp config/files/webpack.config.js.template webpack.config.js
 
 FILE=package.json
 if [ -f "$FILE" ]; then
@@ -25,3 +33,4 @@ else
 fi
 
 yarn
+yarn build
