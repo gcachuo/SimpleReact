bbd69cece635f9b136146a25f725adb30d08846e
Memo Cachú <gcachu.o@gmail.com>
2019-08-07 00:34:05 -0500

    manipulate dirs

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,32 +5,36 @@ mkdir -p interfaces
 mkdir -p styles/components
 mkdir -p ../www
 
-cp config/files/template.htaccess ../.htaccess
+cd -
 
-cp config/files/components/template.index.tsx components/index.tsx
-cp config/files/components/template.app.tsx components/app.tsx
-cp config/files/interfaces/template.toastr.ts interfaces/toastr.ts
-cp config/files/styles/template.app.scss styles/app.scss
+cp files/template.htaccess ../../.htaccess
 
-cp config/files/www/template.htaccess ../www/.htaccess
-cp config/files/www/template.index.html ../www/index.html
-cp config/files/www/template.gitignore ../www/.gitignore
+cp files/components/template.index.tsx ../components/index.tsx
+cp files/components/template.app.tsx ../components/app.tsx
+cp files/interfaces/template.toastr.ts ../interfaces/toastr.ts
+cp files/styles/template.app.scss ../styles/app.scss
 
-cp config/files/template.gitignore .gitignore
-cp config/files/template.imports.ts imports.ts
-cp config/files/template.tsconfig.json tsconfig.json
-cp config/files/template.webpack.config.js webpack.config.js
+cp files/www/template.htaccess ../../www/.htaccess
+cp files/www/template.index.html ../../www/index.html
+cp files/www/template.gitignore ../../www/.gitignore
 
-FILE=package.json
+cp files/template.gitignore ../.gitignore
+cp files/template.imports.ts ../imports.ts
+cp files/template.tsconfig.json ../tsconfig.json
+cp files/template.webpack.config.js ../webpack.config.js
+
+FILE=../package.json
 if [ -f "$FILE" ]; then
     echo "$FILE exists"
 else
-    cp config/files/template.package.json package.json
+    cp files/template.package.json ../package.json
     read -p "Enter the project's name:" project
-    sed -i "s/###PROJECT###/$project/g" package.json
+    sed -i "s/###PROJECT###/$project/g" ../package.json
     read -p "Enter the BASENAME:" BASENAME
-    sed -i "s/###BASENAME###/\/$BASENAME/g" package.json
+    sed -i "s/###BASENAME###/\/$BASENAME/g" ../package.json
 fi
+
+cd ..
 
 yarn
 yarn build
