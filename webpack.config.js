b1cb40ef672a8e77b1968d2b5cc3e9e58e1910d5
Memo Cachu <gcachu.o@gmail.com>
2019-08-26 16:27:01 -0500

    images

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = env => {
             }),
             new CopyPlugin([
                 { from: 'img', to: '../img' },
+                { from: 'images', to: '../images' },
             ]),
         ],
         module: {
