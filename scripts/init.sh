4802f63102e03d26d2dd845f0404a2ab29844d7a
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 15:36:07 -0600

    more files

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,3 +7,9 @@ cp src/lib/files/.htaccess .htaccess
 cp src/lib/files/.gitignore .gitignore
 cp src/lib/files/www/index.html www/index.html
 cp src/lib/files/src/package.json src/package.json
+cp src/lib/files/src/index.ts src/index.ts
+cp src/lib/files/src/components/index.ts src/components/index.ts
+
+cd src || exit
+
+yarn && yarn build
