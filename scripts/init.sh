8578a5d377215c3f4984a2623dccdc5e8b25a0b8
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 15:32:11 -0600

    moved files

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,5 @@ mkdir -p src/components
 
 cp src/lib/files/.htaccess .htaccess
 cp src/lib/files/.gitignore .gitignore
-cp src/lib/files/index.html www/index.html
-cp src/lib/files/package.json src/index.html
+cp src/lib/files/www/index.html www/index.html
+cp src/lib/files/src/package.json src/package.json
