ed97f434df5332472f49ca234597ee2e920efcbb
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 16:22:03 -0600

    more files

--- files/src/index.tsx
+++ files/src/index.tsx
@@ -1,8 +1,15 @@
-import "./components"
+import * as Components from "./components"
 import * as React from "react";
+import * as ReactRouterDOM from "react-router-dom";
+
+declare const BASENAME;
 
 export default class App extends React.Component {
     render() {
-        return <></>
+        return (
+            <ReactRouterDOM.BrowserRouter basename={BASENAME}>
+                <ReactRouterDOM.Route path="/" component={}/>
+            </ReactRouterDOM.BrowserRouter>
+        )
     }
 }
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,12 @@ mkdir -p src/components
 
 cp src/lib/files/.htaccess .htaccess
 cp src/lib/files/.gitignore .gitignore
+
 cp src/lib/files/www/index.html www/index.html
+cp src/lib/files/www/.htaccess www/.htaccess
+
 cp src/lib/files/src/package.json src/package.json
-cp src/lib/files/src/index.ts src/index.ts
+cp src/lib/files/src/index.tsx src/index.tsx
 cp src/lib/files/src/components/index.ts src/components/index.ts
 
 cd src || exit
