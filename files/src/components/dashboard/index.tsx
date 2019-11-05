b3f8379d547098988369d93afa911e8af55c08e9
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 16:54:53 -0600

    fix

--- files/src/components/dashboard/index.tsx
+++ files/src/components/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export class Dashboard extends React.Component() {
+export class Dashboard extends React.Component {
     render() {
         return <>Dashboard</>
     }
--- files/src/index.tsx
+++ files/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactRouterDOM from "react-router-dom";
-import * as Components from "./components"
+import * as Components from "./components";
 
 declare const BASENAME;
 
