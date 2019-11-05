b94c77baa9cbc19e988a3da6969fec2af3c49fbc
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 16:18:40 -0600

    basename

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
+const webpack = require('webpack');
 const path = require('path');
-module.exports = {
-    entry: './lib/entry.tsx',
-    output: {
-        path: path.resolve(__dirname, './../../www/assets'),
-        publicPath: 'www/assets',
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-        ]
+module.exports = env => {
+    return {
+        entry: './lib/entry.tsx',
+        output: {
+            path: path.resolve(__dirname, './../../www/assets'),
+            publicPath: 'www/assets',
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                BASENAME: JSON.stringify(env.BASENAME || '/')
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+            ]
+        }
     }
 };
