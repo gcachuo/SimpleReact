40de19fc596e77bd1cf46f7571e638316b2bcab1
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 15:30:25 -0600

    package.json

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,9 @@
 cd ../../..
 
 mkdir -p www
+mkdir -p src/components
 
 cp src/lib/files/.htaccess .htaccess
 cp src/lib/files/.gitignore .gitignore
 cp src/lib/files/index.html www/index.html
+cp src/lib/files/package.json src/index.html
