3ef90d9a8ab7968ebbb5816f11d18800b7b84d91
Memo Cachu <gcachu.o@gmail.com>
2019-11-05 15:54:51 -0600

    fix

--- entry.tsx
+++ entry.tsx
@@ -1,6 +1,6 @@
 import "../index.ts";
 import * as $ from "jquery";
 import * as React from "react";
-import {ReactDOM} from "react-dom";
+import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<></>, $("#app")[0]);
+ReactDOM.render(<>Hola</>, $("#app")[0]);
