c6811a06cfe37c5ef8283da119b9934023fb277f
Memo Cachu <gcachu.o@gmail.com>
2019-11-06 16:35:25 -0600

    path

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = env => {
     return {
         entry: './lib/entry.tsx',
         output: {
             path: path.resolve(__dirname, './../../www/assets'),
-            publicPath: 'www/assets',
+            publicPath:  path.resolve(__dirname, './../../www/assets'),
             filename: 'bundle.js'
         },
         resolve: {
@@ -14,7 +15,18 @@ module.exports = env => {
         plugins: [
             new webpack.DefinePlugin({
                 BASENAME: JSON.stringify(env.BASENAME || '/')
-            })
+            }),
+            new webpack.ProvidePlugin({
+                jQuery: 'jquery',
+                $: 'jquery'
+            }),
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // all options are optional
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+                ignoreOrder: false, // Enable to remove warnings about conflicting order
+            }),
         ],
         module: {
             rules: [
@@ -22,6 +34,36 @@ module.exports = env => {
                     test: /\.tsx?$/,
                     use: 'ts-loader',
                     exclude: /node_modules/
+                },
+                {
+                    test: /\.html$/,
+                    use: 'html-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.(png|jpg)$/,
+                    loader: 'file-loader?name=img/[name].[ext]'
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        "css-loader"
+                    ],
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/'
+                            }
+                        }
+                    ]
                 },
             ]
         }
